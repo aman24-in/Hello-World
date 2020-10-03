330dbde2776fa03f8cef6fe7feddd4c1980adc01
Aman Sharma <sharma.aman_in@outlook.com>
2020-10-03 23:48:00 +0530

    Update HelloWorld.c
    Add Comments to code

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,7 +1,7 @@
-#include<stdio.h>
+#include<stdio.h> //include library file containing prebuilt functions & keywords
 
-int main(void)
+int main(void)  //writing vois is optional, even without it program will know by default that its void
 {
   printf("Hello, world!");
-  return 0;
+  return 0; //tells program to return control to caller
 }
